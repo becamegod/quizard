4576cd5560635458e38e558618d24279f6514780
HuynhHung256 <huynhduyhung256@gmail.com>
2022-11-27 22:11:57 +0700

    update register

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -7,224 +7,223 @@ import {
   DatePicker,
   notification
 } from "antd";
-import { ExclamationCircleOutlined } from "@ant-design/icons";
+// import { ExclamationCircleOutlined } from "@ant-design/icons";x
 import { Content } from "antd/lib/layout/layout";
 import Title from "antd/lib/typography/Title";
-import React from "react";
+import { React, useState, useEffect } from "react";
 import authService from "../../services/auth";
+import VerifyForm from "../VerifyForm";
 
 export default function RegisterForm() {
-  const [finish, setFinish] = React.useState(false);
-  const [user, setUser] = React.useState({
-    name: "",
-    email: "",
-    password: "",
-    dob: "",
-    gender: ""
-  });
-
+  const [stage, setStage] = useState(0);
+  const [user, setUser] = useState({});
   const onFinish = (values) => {
     console.log("Success:", values);
     setUser({ ...user, ...values });
-    if (finish) {
-      console.log("registering...", user);
-      authService
-        .register(user)
-        .then((res) => {
-          console.log("res ", res);
-          localStorage.setItem("user", res.user);
-          window.location.href = "/dashboard";
-        })
-        .catch((err) => {
-          console.log("err ", err);
-          notification.error({
-            message: "Error",
-            description: err.message,
-            icon: <ExclamationCircleOutlined style={{ color: "#FF4D4F" }} />,
-            style: {
-              backgroundColor: "#FFF1F0",
-              borderRadius: "10px"
-            }
-          });
-          setFinish(false);
-        });
-      return;
-    }
-    setFinish(true);
+    setStage(stage + 1);
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
-  if (finish) {
-    return (
-      <Card className="round">
-        <Title>REGISTER</Title>
-        <Form
-          layout="vertical"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Name"
-            name="name"
-            validateTrigger="onBlur"
-            rules={[
-              {
-                required: true,
-                message: "Please input your name!"
-              },
-              {
-                max: 200,
-                message: "Name must be less than 200 characters long!"
-              }
-            ]}
+
+  useEffect(() => {
+    console.log("user", user);
+    console.log("stage", stage);
+    if (stage === 2) {
+      authService
+        .register(user)
+        .then((res) => {
+          if (res.status === 201) {
+            notification.success({
+              message: "Register Success",
+              description: "Please check your email to verify your account"
+            });
+            window.location.href = "/login";
+          }
+        })
+        .catch((err) => {
+          notification.error({
+            message: "Register Failed",
+            description: err.response.data.message || "Unknown Error"
+          });
+          setStage(0);
+        });
+    }
+  }, [stage]);
+
+  switch (stage) {
+    case 1:
+      return (
+        <Card className="round">
+          <Title>REGISTER</Title>
+          <Form
+            layout="vertical"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
           >
-            <Input className="round" />
-          </Form.Item>
-          <Form.Item
-            label="Gender"
-            name="gender"
-            rules={[
-              {
-                required: true,
-                message: "Please select your gender!"
-              }
-            ]}
-          >
-            <Select
-              className="round input-field"
-              style={{ width: "100%", borderRadius: "5px" }}
-              options={[
+            <Form.Item
+              label="Name"
+              name="name"
+              validateTrigger="onBlur"
+              rules={[
                 {
-                  value: "Male",
-                  label: "Male"
+                  required: true,
+                  message: "Please input your name!"
                 },
                 {
-                  value: "Female",
-                  label: "Female"
+                  max: 200,
+                  message: "Name must be less than 200 characters long!"
                 }
               ]}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Date of birth"
-            name="dob"
-            rules={[
-              {
-                required: true,
-                message: "Please select your date of birth!"
-              },
-              () => ({
-                validator(rule, value) {
-                  if (value && value.isAfter(new Date())) {
-                    // eslint-disable-next-line prefer-promise-reject-errors
-                    return Promise.reject("Date of birth must be in the past!");
-                  }
-                  return Promise.resolve();
-                }
-              })
-            ]}
-          >
-            <DatePicker className="round input-field" value={new Date()} />
-          </Form.Item>
-          <Form.Item>
-            <Content className="register-container">
-              <Button
-                type="primary"
-                size="large"
-                className="round register-btn"
-                htmlType="submit"
-              >
-                Register
-              </Button>
-            </Content>
-          </Form.Item>
-        </Form>
-      </Card>
-    );
-  }
-  return (
-    <Card className="round">
-      <Title>REGISTER</Title>
-      <Form
-        layout="vertical"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          validateTrigger="onBlur"
-          rules={[
-            {
-              type: "email",
-              message: "The input is not valid E-mail!"
-            },
-            {
-              required: true,
-              message: "Please input your E-mail!"
-            }
-          ]}
-        >
-          <Input className="round" />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          validateTrigger="onBlur"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!"
-            },
-            {
-              min: 8,
-              max: 16,
-              message: "Password must be between 8 and 16 characters long!"
-            }
-          ]}
-        >
-          <Input.Password className="round" />
-        </Form.Item>
-        <Form.Item
-          label="Confirm password"
-          name="confirm-password"
-          validateTrigger="onBlur"
-          rules={[
-            {
-              required: true,
-              message: "Please confirm your password!"
-            },
-            ({ getFieldValue }) => ({
-              validator(rule, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-                // eslint-disable-next-line prefer-promise-reject-errors
-                return Promise.reject(
-                  "The two passwords that you entered do not match!"
-                );
-              }
-            })
-          ]}
-        >
-          <Input.Password className="round" />
-        </Form.Item>
-        <Form.Item>
-          <Content className="register-container">
-            <Button
-              htmlType="submit"
-              type="primary"
-              className="round register-btn"
-              size="large"
             >
-              Continue
-            </Button>
-          </Content>
-        </Form.Item>
-      </Form>
-    </Card>
-  );
+              <Input className="round" />
+            </Form.Item>
+            <Form.Item
+              label="Gender"
+              name="gender"
+              rules={[
+                {
+                  required: true,
+                  message: "Please select your gender!"
+                }
+              ]}
+            >
+              <Select
+                className="round input-field"
+                style={{ width: "100%", borderRadius: "5px" }}
+                options={[
+                  {
+                    value: "Male",
+                    label: "Male"
+                  },
+                  {
+                    value: "Female",
+                    label: "Female"
+                  }
+                ]}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Date of birth"
+              name="dob"
+              rules={[
+                {
+                  required: true,
+                  message: "Please select your date of birth!"
+                },
+                () => ({
+                  validator(rule, value) {
+                    if (value && value.isAfter(new Date())) {
+                      // eslint-disable-next-line prefer-promise-reject-errors
+                      return Promise.reject("Date of birth must be in the past!");
+                    }
+                    return Promise.resolve();
+                  }
+                })
+              ]}
+            >
+              <DatePicker className="round input-field" value={new Date()} />
+            </Form.Item>
+            <Form.Item>
+              <Content className="register-container">
+                <Button
+                  type="primary"
+                  size="large"
+                  className="round register-btn"
+                  htmlType="submit"
+                >
+                  Register
+                </Button>
+              </Content>
+            </Form.Item>
+          </Form>
+        </Card>
+      );
+    case 2:
+      return <VerifyForm />;
+    default:
+      return (
+        <Card className="round">
+          <Title>REGISTER</Title>
+          <Form
+            layout="vertical"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Email"
+              name="email"
+              validateTrigger="onBlur"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!"
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!"
+                }
+              ]}
+            >
+              <Input className="round" />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              validateTrigger="onBlur"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!"
+                },
+                {
+                  min: 8,
+                  max: 16,
+                  message: "Password must be between 8 and 16 characters long!"
+                }
+              ]}
+            >
+              <Input.Password className="round" />
+            </Form.Item>
+            <Form.Item
+              label="Confirm password"
+              name="confirm-password"
+              validateTrigger="onBlur"
+              rules={[
+                {
+                  required: true,
+                  message: "Please confirm your password!"
+                },
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    // eslint-disable-next-line prefer-promise-reject-errors
+                    return Promise.reject(
+                      "The two passwords that you entered do not match!"
+                    );
+                  }
+                })
+              ]}
+            >
+              <Input.Password className="round" />
+            </Form.Item>
+            <Form.Item>
+              <Content className="register-container">
+                <Button
+                  htmlType="submit"
+                  type="primary"
+                  className="round register-btn"
+                  size="large"
+                >
+                  Continue
+                </Button>
+              </Content>
+            </Form.Item>
+          </Form>
+        </Card>
+      );
 }
--- src/components/VerifyForm/index.jsx
+++ src/components/VerifyForm/index.jsx
@@ -1,8 +1,52 @@
 import { React } from "react";
-import { Row, Col, Pagination, Space } from "antd";
-import PropTypes from "prop-types";
-import GroupCard from "../GroupCard";
-import groupService from "../../services/groups";
+import { Input, Form, Card, Typography, Button } from "antd";
 
 export default function VerifyForm() {
+  const { Title } = Typography;
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Card className="round">
+      <Title>VERIFY</Title>
+      <Form
+        layout="vertical"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          name="code"
+          validateTrigger="onBlur"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!"
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!"
+            }
+          ]}
+        >
+          <Input className="round" />
+        </Form.Item>
+        <Form.Item style={{ textAlign: "center" }}>
+          <Button
+            htmlType="submit"
+            type="primary"
+            className="round"
+            size="large"
+          >
+            Verify
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 }
