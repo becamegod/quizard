e788fe572890287d2c8e84bfa54136e2ad02dff1
NguyenQuocThong <63840452+NguyenQuocThong2603@users.noreply.github.com>
2022-12-01 00:19:10 +0700

    css for profile page

--- src/components/UserInformationForm/index.jsx
+++ src/components/UserInformationForm/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   Row,
@@ -8,17 +8,42 @@ import {
   Button,
   Input,
   Select,
-  notification
+  notification,
+  Spin
 } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 import moment from "moment";
-import { updateProfile } from "../../services/user.service";
+import { updateProfile, getProfile } from "../../services/user.service";
+import NavBar from "../NavBar";
 
-export default function UserInformation(props) {
-  const { userData } = { ...props };
+// eslint-disable-next-line react/prop-types
+export default function UserInformationForm() {
+  const [userData, setUserData] = useState(null);
+
+  async function fetchData() {
+    try {
+      return getProfile();
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+  useEffect(() => {
+    fetchData().then((res) => {
+      setUserData((prev) => {
+        return {
+          ...prev,
+          ...res.data
+        };
+      });
+    });
+  }, []);
+
+  const antIcon = (
+    <LoadingOutlined style={{ fontSize: 48, color: "red" }} spin />
+  );
   const onFinish = async (values) => {
     const { name, email, dob, gender } = { ...values };
     const dobString = moment(dob).format("DD/MM/YYYY");
-    console.log(dobString);
     const user = {
       email,
       name,
@@ -26,7 +51,8 @@ export default function UserInformation(props) {
       dob: dobString
     };
     try {
-      await updateProfile(user);
+      const updateUser = await updateProfile(user);
+      setUserData(updateUser);
       notification.success({
         message: "Edit information succeed"
       });
@@ -37,61 +63,75 @@ export default function UserInformation(props) {
       throw new Error(err);
     }
   };
-  return (
-    <Card className="profile-card-container">
-      <Row className="row-container">
-        <Col className="avatar" span={8} />
-        <Col className="information" span={16}>
-          <Row className="information-container">
-            <Form
-              className="information-form"
-              onFinish={onFinish}
-              labelCol={{ span: 4 }}
-              initialValues={{
-                email: `${userData.email}`,
-                name: `${userData.name}`,
-                gender: `${userData.gender}`,
-                dob: moment(userData.dob, "DD/MM/YYYY")
-              }}
-            >
-              <Form.Item label="Email" name="email">
-                <Input className="round" disabled />
-              </Form.Item>
-              <Form.Item label="Fullname" name="name">
-                <Input className="round" required />
-              </Form.Item>
-              <Form.Item label="Gender" name="gender">
-                <Select
-                  className="round"
-                  options={[
-                    {
-                      value: "Male",
-                      label: "Male"
-                    },
-                    {
-                      value: "Female",
-                      label: "Female"
-                    }
-                  ]}
-                  required
-                />
-              </Form.Item>
-              <Form.Item label="Date of birth" name="dob">
-                <DatePicker format="DD/MM/YYYY" required />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  className="edit-profile-button"
-                  type="primary"
-                  htmlType="submit"
+  if (userData) {
+    return (
+      <>
+        <NavBar />
+        <Card className="profile-card-container">
+          <Row className="row-container">
+            <Col className="avatar" span={8} />
+            <Col className="information" span={16}>
+              <Row className="information-container">
+                <Form
+                  className="information-form"
+                  onFinish={onFinish}
+                  initialValues={{
+                    email: `${userData.email}`,
+                    name: `${userData.name}`,
+                    gender: `${userData.gender}`,
+                    dob: moment(userData.dob, "DD/MM/YYYY")
+                  }}
                 >
-                  Edit information
-                </Button>
-              </Form.Item>
-            </Form>
+                  <Form.Item label="Email" name="email">
+                    <Input className="round" disabled />
+                  </Form.Item>
+                  <Form.Item label="Fullname" name="name">
+                    <Input className="round" required />
+                  </Form.Item>
+                  <Form.Item label="Gender" name="gender">
+                    <Select
+                      className="round"
+                      options={[
+                        {
+                          value: "Male",
+                          label: "Male"
+                        },
+                        {
+                          value: "Female",
+                          label: "Female"
+                        }
+                      ]}
+                      required
+                    />
+                  </Form.Item>
+                  <Form.Item label="Date of birth" name="dob">
+                    <DatePicker
+                      className="round"
+                      format="DD/MM/YYYY"
+                      required
+                    />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button
+                      className="edit-profile-button"
+                      type="primary"
+                      htmlType="submit"
+                    >
+                      Edit information
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Row>
+            </Col>
           </Row>
-        </Col>
-      </Row>
-    </Card>
+        </Card>
+      </>
+    );
+  }
+  return (
+    <>
+      <NavBar />
+      <Spin indicator={antIcon} />
+    </>
   );
 }
