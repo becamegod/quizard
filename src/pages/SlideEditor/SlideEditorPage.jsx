1792ec5643954202ec17aa769b2e3c91318a6da0
NguyenQuocThong <63840452+NguyenQuocThong2603@users.noreply.github.com>
2022-12-08 21:23:18 +0700

    beautify edit presentation

--- src/pages/SlideEditor/SlideEditorPage.jsx
+++ src/pages/SlideEditor/SlideEditorPage.jsx
@@ -113,7 +113,7 @@ export default function SlideEditorPage() {
               onClick={handleOnClickBackButton}
             />
             <Input
-              className="input-name-presentation"
+              className="input-name-presentation round"
               defaultValue={presentation.name}
               onChange={(e) => handleChangeName(e)}
             />
@@ -152,7 +152,7 @@ export default function SlideEditorPage() {
           <Col className="chart-screen" span={12}>
             <ChartScreen selectedSlide={presentation.slides[selectedId]} />
           </Col>
-          <Col className="choice-container" span={7}>
+          <Col className="choice-container" span={8}>
             <ChoiceCard
               selectedSlide={presentation.slides[selectedId]}
               handleChangeOption={handleChangeOption}
