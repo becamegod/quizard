0bc6dcf470c6acf1c06195f469a9b5b9a3a1371e
Thành <37612705+becamegod@users.noreply.github.com>
2022-12-18 21:36:22 +0700

    Turn off socket

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,5 +1,5 @@
-import React from "react";
-import socketio from "socket.io-client";
+// import React from "react";
+// import socketio from "socket.io-client";
 
-export const socket = socketio.connect("localhost:5000");
-export const SocketContext = React.createContext();
+// export const socket = socketio.connect("localhost:5000");
+// export const SocketContext = React.createContext();
