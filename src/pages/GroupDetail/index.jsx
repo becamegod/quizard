5b07330cfc2d639f2fca24e128ec117856e71dd4
Thành <37612705+becamegod@users.noreply.github.com>
2022-12-18 13:28:49 +0700

    Fix group detail page

--- src/pages/GroupDetail/index.jsx
+++ src/pages/GroupDetail/index.jsx
@@ -1,23 +1,23 @@
 import { React } from "react";
 import { Row, Col, Space } from "antd";
 import GroupInfoCard from "./GroupInfoCard";
-import DashboardLayout from "../Dashboard";
+// import DashboardLayout from "../Dashboard";
 import GroupMemberCard from "./GroupMemberCard";
 import "./index.css";
 import PresentationCard from "./PresentationCard";
 
 export default function GroupDetailPage() {
   return (
-    <DashboardLayout>
-      <Row justify="center">
-        <Col span={18}>
-          <Space direction="vertical" size={32} style={{ width: "100%" }}>
-            <GroupInfoCard />
-            <PresentationCard />
-            <GroupMemberCard />
-          </Space>
-        </Col>
-      </Row>
-    </DashboardLayout>
+    // <DashboardLayout>
+    <Row justify="center">
+      <Col span={18}>
+        <Space direction="vertical" size={32} style={{ width: "100%" }}>
+          <GroupInfoCard />
+          <PresentationCard />
+          <GroupMemberCard />
+        </Space>
+      </Col>
+    </Row>
+    // </DashboardLayout>
   );
 }
